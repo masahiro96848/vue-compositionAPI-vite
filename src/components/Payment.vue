c01bb342715ce39d6d83bffb101e1537eae5351b
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-20 14:02:36 +0900

    feat: Paymentコンポーネントを作成

---
+++ src/components/Payment.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="container">
+        <h1>最近の支出</h1>
+        <div class="payment">
+            <label for="">カレーライス</label>
+            <label for="">400円</label>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.payment {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 80px;
+    width: 400px;
+    background-color: aliceblue;
+}
+
+label {
+    font-size: 20px;
+}
+</style>
