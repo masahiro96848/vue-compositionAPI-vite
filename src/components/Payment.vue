19abd8caef8d132e750297f31b2ba639f74676c7
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-20 14:20:31 +0900

    feat: v-on処理の動作確認

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,13 +1,33 @@
 <script setup lang="ts">
+    const itemName1 = 'Desk'
+    const itemName2 = 'Bike'
 
+    const price1 = 40000
+    const price2 = 20000
+
+    const url1 = 'https://yutaro-blog.net/2022/03/04/freelance-web-engineer-2/#index_id11'
+    const buy = (itemName: string) => {
+        alert('購入しますか？' + itemName + '?')
+    }
+
+    const input = (event: any) => {
+        console.log('ddddd', event.target.value)
+    }
 </script>
 
 <template>
     <div class="container">
         <h1>最近の支出</h1>
         <div class="payment">
-            <label for="">カレーライス</label>
-            <label for="">400円</label>
+            <input v-on:input="input">
+            <label for="">{{ itemName1 }}</label>
+            <label for="">{{ price1 }}</label>
+            <a v-bind:href="url1">bought at</a>
+            <button v-on:click="buy(itemName1)">Buy</button>
+        </div>
+        <div class="payment">
+            <label for="">{{ itemName2 }}</label>
+            <label for="">{{ price2 }}</label>
         </div>
     </div>
 </template>
