3410b4d479832bbe365e0044a73a3347611c251b
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-28 18:31:20 +0900

    feat: emitを使って実装

---
+++ src/components/TweetList.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { defineProps ,defineEmits } from 'vue';
+type Tweet = {
+    id: number,
+    description: string,
+}
+
+type Props = {
+    tweets: Tweet[]
+}
+
+
+
+defineProps<Props>()
+const emit = defineEmits(['delete-tweet'])
+const deleteTweet = (id: number) => {
+    emit('delete-tweet', id)
+}
+
+
+</script>
+
+<template>
+    <li v-for="tweet in tweets" class="tweet-list" :key="tweet.id">
+        <span>{{tweet.description}}</span>
+        <button @click="deleteTweet(tweet.id)" class="delete-button">delete</button>
+    </li>
+</template>
+
+<style scoped>
+.tweet-list {
+    list-style: none;
+    margin-bottom: 12px;
+    border-radius: 4px;
+    font-size: 12px;
+    display: flex;
+    justify-content: space-between;
+    background-color: rgb(204, 219, 233);
+    padding: 8px 20px;
+    width: 300px;
+}
+
+.save-button {
+    color: #fff;
+    font-weight: bold;
+    background-color: #68c9c9;
+    border-radius: 2px;
+    border: none;
+    width: 60px;
+    height: 22px;
+}
+
+.delete-button {
+    color: #fff;
+    font-weight: bold;
+    background-color: #dc9421;
+    border-radius: 2px;
+    border: none;
+    width: 60px;
+    height: 22px;
+}
+</style>
