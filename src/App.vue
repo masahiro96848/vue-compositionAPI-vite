2835e43ddd8895565547e335736b12d34d675840
masahiro96848 <feetwcpic9436@gmail.com>
2022-09-28 16:19:27 +0900

    feat: 静的なリストを作成

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,18 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-
+import Tweet from './components/Tweet.vue';
 </script>
 
 <template>
   <div>
-    
+    <Tweet />
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+#app {
+  text-align: center;
+  color: #2c3e50;
 }
 </style>
